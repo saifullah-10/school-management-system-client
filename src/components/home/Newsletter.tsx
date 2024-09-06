bda1fc168f3005393c80cc56dcd58d4f54298c51
Zahid Hasan <imzahidhasan.mail@gmail.com>
2024-09-06 23:18:14 +0600

    completed newsletter design

--- src/components/home/Newsletter.tsx
+++ src/components/home/Newsletter.tsx
@@ -1,3 +1,34 @@
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
 export default function Newsletter() {
-  return <div>Newsletter</div>;
+  return (
+    <div className="container mx-auto">
+      <div className="bg-purple-700 p-4 h-52 sm:p-8 rounded-lg flex flex-col sm:flex-row items-center justify-between relative overflow-hidden">
+        <div className="flex flex-col w-full sm:w-1/2 space-y-2 mb-4 sm:mb-0 text-center sm:text-left font-bold ">
+          <h2 className="text-white text-2xl sm:text-3xl font-bold">
+            Join Our Newsletter
+          </h2>
+          <p className="text-purple-200  text-sm sm:text-base">
+            Subscribe our newsletter to get our latest update & news.
+          </p>
+        </div>
+        <div className="w-full sm:w-1/2 flex justify-center items-center">
+          <div className="relative w-full max-w-md">
+            <Input
+              type="email"
+              placeholder="Enter your email:"
+              className="pr-28 w-full"
+            />
+            <Button
+              className="absolute  right-0 top-0 rounded-l-none"
+              variant="secondary"
+            >
+              Submit Now
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
