8332f6845017f8d1b97d6706c5557474179954c0
samwaseee <samwaseee@gmail.com>
2024-09-07 20:32:11 +0600

    why choose us section animation added

---
+++ src/components/animation/home/whyChooseUs/UpDownDotBox.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import React from 'react';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+const UpDownDotBox = () => {
+    return (
+        <motion.div
+            animate={{ y: [0, -20, 0] }}
+            transition={{ duration: 1, repeat: Infinity }}
+            className="absolute -top-20 -right-20 -z-10"
+        >
+            <Image
+                src="/assets/images/shape-1-3.png"
+                alt="dot png"
+                width={277}
+                height={271}
+            />
+        </motion.div>
+    );
+};
+
+export default UpDownDotBox;
