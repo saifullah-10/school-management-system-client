9dfa2b24647655051467aa19e91d4d06e51acd7c
samwaseee <samwaseee@gmail.com>
2024-09-07 12:26:57 +0600

    intial courses card added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Montserrat: "'Montserrat', sans-serif",
+        Poppins: "'Poppins', sans-serif"
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
