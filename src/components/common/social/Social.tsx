0a6f1b3b618cc7394792918276c4ac68526c3a94
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-09-07 20:11:12 +0600

    dashboard

---
+++ src/components/common/social/Social.tsx
@@ -0,0 +1,10 @@
+import { FaGooglePlusG } from "react-icons/fa";
+
+const Social = () => {
+    return (
+        <div  className="btn w-full bg-transparent border-black "><FaGooglePlusG className="text-2xl" /> Continue with Google </div>
+
+    );
+};
+
+export default Social;
