ea60c3da85d74a59a0d00f2cad91e9eb8d9a81b5
samwaseee <samwaseee@gmail.com>
2024-09-08 15:36:33 +0600

    courses page animation added

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,8 +1,16 @@
+import Linear from "@/components/animation/courses/Linear";
+import LinearWave from "@/components/animation/courses/LinearWave";
+import Rotate from "@/components/animation/courses/Rotate";
+import RotateFast from "@/components/animation/courses/RotateFast";
 
 const page = () => {
     return (
-        <div className="h-screen text-5xl">
-            Courses
+        <div className="bg-[url('/assets/images/breadcrumb.jpg')] py-40 relative">
+            <h1 className="text-6xl text-center font-Montserrat font-bold">All Courses</h1>
+            <Linear/>
+            <LinearWave/>
+            <Rotate/>
+            <RotateFast/>
         </div>
     );
 };
