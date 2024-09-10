bb24c676a8f838752fa17f7e44eff209d066b7ce
saifullah-10 <saifullah.bus@gmail.com>
2024-09-10 18:03:16 +0600

    connect to deployed server

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://school-management-system-server-ashen.vercel.app";
 
 export const register = async (email: string, password: string) => {
   return axios.post(
