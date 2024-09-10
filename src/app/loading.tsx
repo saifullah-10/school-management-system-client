f5de4190ac8100de37c83a6c756205456340f74e
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-09-10 21:56:37 +0600

    update routes

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -6,7 +6,7 @@ import loader from '../../public/assets/allLoading.json'
 const Preloader = () => {
     return (
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
-         <Lottie animationData={loader} />;
+         <Lottie animationData={loader} />
     </div>
     );
 };
--- src/components/dashboard/Sidebar.tsx/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx/Sidebar.tsx
@@ -36,6 +36,7 @@ const Sidebar = () => {
     setIsTeachersOpen(!isTeachersOpen); // Toggle submenu open/close
   };
 
+
   return (
     <ul className="mt-4 text-[17px]">
       <Link href="/dashboard">
@@ -107,6 +108,7 @@ const Sidebar = () => {
         )}
       </li>
 
+      
       <Link href="/dashboard/classRoutine">
         <li
           className={`py-2 px-4 flex items-center gap-2 hover:text-white hover:bg-[#704FE6] border-y-2 border-white ${
@@ -131,7 +133,29 @@ const Sidebar = () => {
           Attendance
         </li>
       </Link>
+      <Link href="/dashboard/courses">
+        <li
+          className={`py-2 px-4 flex items-center gap-2 hover:text-white hover:bg-[#704FE6] border-y-2 border-white ${
+            pathname === "/dashboard/courses"
+              ? "bg-[#704FE6] text-white"
+              : ""
+          }`}
+        >
+          <FaRegCalendarAlt />
+          Courses
+        </li>
+      </Link>
 
+      <Link href="/dashboard/exam">
+        <li
+          className={`py-2 px-4 flex items-center gap-2 hover:text-white hover:bg-[#704FE6] border-y-2 border-white ${
+            pathname === "/dashboard/exam" ? "bg-[#704FE6] text-white" : ""
+          }`}
+        >
+          <ImProfile />
+          Exam
+        </li>
+      </Link>
       <Link href="/dashboard/profile">
         <li
           className={`py-2 px-4 flex items-center gap-2 hover:text-white hover:bg-[#704FE6] border-y-2 border-white ${
