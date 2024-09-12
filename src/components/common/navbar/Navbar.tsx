20a9ad74bed90a7a3b57a308fc119487b07828aa
saifullah-10 <saifullah.bus@gmail.com>
2024-09-12 11:12:27 +0600

    update navbar

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -1,23 +1,54 @@
 "use client";
 
+import { useAuth } from "@/contextProvider/ContextProvider";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState } from "react";
 import { IoClose, IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
+  const { user } = useAuth();
   const navigate = usePathname();
-  console.log(navigate);
-    const Links = <>
-        <Link href="/" className={`hover:text-[#704FE6]  ${navigate === '/' ? 'nav-active' : ''}`}>Home</Link>
-        <Link href="/aboutUs" className={`hover:text-[#704FE6]  ${navigate === '/aboutUs' ? 'nav-active' : ''}`}>About Us</Link>
-        <Link href="/courses" className={`hover:text-[#704FE6]  ${navigate === '/courses' ? 'nav-active' : ''}`}>Courses</Link>
-        <Link href="/contact" className={`hover:text-[#704FE6]  ${navigate === '/contact' ? 'nav-active' : ''}`}>Contact</Link>
+  console.log(user);
+  const Links = (
+    <>
+      <Link
+        href="/"
+        className={`hover:text-[#704FE6]  ${
+          navigate === "/" ? "nav-active" : ""
+        }`}
+      >
+        Home
+      </Link>
+      <Link
+        href="/aboutUs"
+        className={`hover:text-[#704FE6]  ${
+          navigate === "/aboutUs" ? "nav-active" : ""
+        }`}
+      >
+        About Us
+      </Link>
+      <Link
+        href="/courses"
+        className={`hover:text-[#704FE6]  ${
+          navigate === "/courses" ? "nav-active" : ""
+        }`}
+      >
+        Courses
+      </Link>
+      <Link
+        href="/contact"
+        className={`hover:text-[#704FE6]  ${
+          navigate === "/contact" ? "nav-active" : ""
+        }`}
+      >
+        Contact
+      </Link>
     </>
+  );
 
-    const [menu, setMenu] = useState(false)
-    const menuRef = useRef<HTMLDivElement | null>(null);
-
+  const [menu, setMenu] = useState(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
 
   const handleClickOutside = (event: MouseEvent) => {
     if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
@@ -35,12 +66,15 @@ const Navbar = () => {
   }, []);
 
   if (navigate?.includes("dashboard")) {
-    return ;
+    return;
   }
 
   return (
     <div
-      className={`border-b-[1px] border-[#704FE6] sticky top-0 z-50 bg-white  ${navigate === "/login" || navigate === "/register" ? "hidden" : ""}`}>
+      className={`border-b-[1px] border-[#704FE6] sticky top-0 z-50 bg-white  ${
+        navigate === "/login" || navigate === "/register" ? "hidden" : ""
+      }`}
+    >
       <nav className="max-w-screen-2xl md:w-11/12  flex justify-between items-center mx-auto py-4 px-2">
         {/* Mobile menu */}
         <div className="min-[850px]:hidden " onClick={() => setMenu(!menu)}>
@@ -53,7 +87,9 @@ const Navbar = () => {
 
         {/* Left */}
         <Link href={"/"} className=" font-bold flex  items-center">
-          <h1 className=" text-xl min-[360px]:text-2xl transform transition-transform  hover:scale-110 cursor-pointer">Starlight <span className="text-[#704FE6]">University</span></h1>
+          <h1 className=" text-xl min-[360px]:text-2xl transform transition-transform  hover:scale-110 cursor-pointer">
+            Starlight <span className="text-[#704FE6]">University</span>
+          </h1>
         </Link>
         {/* Center */}
         <div className=" absolute left-1/2 -translate-x-1/2 flex gap-6 text-base text-center font-medium max-[850px]:hidden">
@@ -69,7 +105,10 @@ const Navbar = () => {
       {/* Mobile nav */}
       <div
         ref={menuRef}
-        className={`flex flex-col nav-link absolute bg-white text-center space-y-4 w-full  border-y-2  p-5 min-[850px]:hidden ${menu ? "" : "hidden"}`}>
+        className={`flex flex-col nav-link absolute bg-white text-center space-y-4 w-full  border-y-2  p-5 min-[850px]:hidden ${
+          menu ? "" : "hidden"
+        }`}
+      >
         {Links}
       </div>
     </div>
