357f00715181d5cb293112fc273ec33103aab30a
saifullah-10 <saifullah.bus@gmail.com>
2024-09-12 11:25:39 +0600

    update routing logic

--- src/privateRoute/PrivateRoute.tsx
+++ src/privateRoute/PrivateRoute.tsx
@@ -1,38 +1,13 @@
 "use client";
 
-import { fetchProtectedData } from "@/utils/api/api";
-import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { useAuth } from "@/contextProvider/ContextProvider";
 
 const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const router = useRouter();
-  const [user, setUser] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const protectedData = async () => {
-    try {
-      const response = await fetchProtectedData();
-      if (response instanceof Error) {
-        router.push("/login");
-        setUser(false);
-        setLoading(false);
-      } else {
-        if (response?.status === 200) {
-          setUser(true);
-          setLoading(false);
-        }
-      }
-    } catch (err) {
-      setUser(false);
-      setLoading(false);
-    }
-  };
-  protectedData();
-
-  if (loading) {
-    return <div>loading.....</div>;
-  }
+  const { user } = useAuth();
 
   if (!user) {
     router.push("/login");
