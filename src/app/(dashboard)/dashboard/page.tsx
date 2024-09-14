3e814a2241f7d20bf4b32c0b4b34b0cd56fba337
saifullah-10 <saifullah.bus@gmail.com>
2024-09-14 20:34:53 +0600

    update context setup

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,17 +1,22 @@
-'use client'
-import {  useAuthFromContext } from "@/contextProvider/ContextProvider";
+"use client";
+import {
+  ContextProvider,
+  useAuthFromContext,
+} from "@/contextProvider/ContextProvider";
 import Admin from "@/pages/dashboard/admin/Admin";
 import StudentDashboard from "@/pages/dashboard/student/StudentDashboard";
 import TeacherDashboard from "@/pages/dashboard/teacher/TeacherDashboard";
 
 const Dashboard = () => {
-  const { user } =  useAuthFromContext();
+  const { user } = useAuthFromContext();
 
   return (
     <div>
-      {user?.role === 'admin' && <Admin></Admin>}
-      {user?.role === "student" && <StudentDashboard></StudentDashboard>}
-      {user?.role === 'teacher' && <TeacherDashboard></TeacherDashboard>}
+      <ContextProvider>
+        {user?.role === "admin" && <Admin></Admin>}
+        {user?.role === "student" && <StudentDashboard></StudentDashboard>}
+        {user?.role === "teacher" && <TeacherDashboard></TeacherDashboard>}
+      </ContextProvider>
     </div>
   );
 };
