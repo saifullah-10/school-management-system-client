40b8840e447dc4a0bc7ce8401ef47132543e70fe
saifullah-10 <saifullah.bus@gmail.com>
2024-09-15 10:35:43 +0600

    fix tanstack bug

--- src/components/dashboard/adminPath/NoticeBoard.tsx
+++ src/components/dashboard/adminPath/NoticeBoard.tsx
@@ -1,65 +1,70 @@
-"use client";
-import { Card, CardContent } from "@/components/ui/card";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { useAuth } from "@/contextProvider/ContextProvider";
-import { getNoticeData } from "@/utils/api/noticeApis";
-import timeAgo from "@/utils/globalFunction/timeAgoFn";
-import { NoticeInputs } from "@/utils/types/noticeTypes";
-import { useQuery } from "@tanstack/react-query";
+// "use client";
+// import { Card, CardContent } from "@/components/ui/card";
+// import { ScrollArea } from "@/components/ui/scroll-area";
+// import { useAuth } from "@/contextProvider/ContextProvider";
+// import { getNoticeData } from "@/utils/api/noticeApis";
+// import timeAgo from "@/utils/globalFunction/timeAgoFn";
+// import { NoticeInputs } from "@/utils/types/noticeTypes";
+// import { useQuery } from "@tanstack/react-query";
 
-const NoticeBoard = () => {
-  const { data, isLoading } = useQuery({
-    queryKey: ["get-notice-common-data"],
-    queryFn: () => getNoticeData(),
-  });
-  function getRandomColorCode(colorCodes: string[]) {
-    const randomIndex = Math.floor(Math.random() * colorCodes.length);
+// const NoticeBoard = () => {
+//   const { data, isLoading } = useQuery({
+//     queryKey: ["get-notice-common-data"],
+//     queryFn: () => getNoticeData(),
+//   });
+//   function getRandomColorCode(colorCodes: string[]) {
+//     const randomIndex = Math.floor(Math.random() * colorCodes.length);
 
-    return colorCodes[randomIndex];
-  }
+//     return colorCodes[randomIndex];
+//   }
 
-  const colorCodes = ["#FF5733", "#33FF57", "#3357FF", "#F1C40F", "#9B59B6"];
-  const auth = useAuth();
-  if (auth === null) {
-    return <div>loading</div>;
-  }
-  const { user } = auth;
-  const randomColor = getRandomColorCode(colorCodes);
-  if (isLoading) {
-    <div>loading in notice</div>;
-  }
-  return (
-    <div>
-      <div className="">
-        <div className="max-w-md mx-auto p-4">
-          <h1 className="text-2xl font-bold mb-4">Notice Board</h1>
+//   const colorCodes = ["#FF5733", "#33FF57", "#3357FF", "#F1C40F", "#9B59B6"];
+//   const auth = useAuth();
+//   if (auth === null) {
+//     return <div>loading</div>;
+//   }
+//   const { user } = auth;
+//   const randomColor = getRandomColorCode(colorCodes);
+//   if (isLoading) {
+//     <div>loading in notice</div>;
+//   }
+//   return (
+//     <div>
+//       <div className="">
+//         <div className="max-w-md mx-auto p-4">
+//           <h1 className="text-2xl font-bold mb-4">Notice Board</h1>
 
-          <ScrollArea className="h-[305px]">
-            {data?.map((notice: NoticeInputs) => {
-              const convertDate = new Date(notice.posted).toDateString();
-              return (
-                <Card key={notice._id} className="mb-4">
-                  <CardContent className="p-4">
-                    <div
-                      className={`inline-block px-3 py-1 rounded-full text-sm text-white ${randomColor} mb-2`}
-                    >
-                      {convertDate}
-                    </div>
-                    <p className="text-gray-800 text-sm mb-2">
-                      {notice.content}
-                    </p>
-                    <p className="text-sm text-gray-600">
-                      {user?.username} / {timeAgo(convertDate)}
-                    </p>
-                  </CardContent>
-                </Card>
-              );
-            })}
-          </ScrollArea>
-        </div>
-      </div>
-    </div>
-  );
+//           <ScrollArea className="h-[305px]">
+//             {data?.map((notice: NoticeInputs) => {
+//               const convertDate = new Date(notice.posted).toDateString();
+//               return (
+//                 <Card key={notice._id} className="mb-4">
+//                   <CardContent className="p-4">
+//                     <div
+//                       className={`inline-block px-3 py-1 rounded-full text-sm text-white ${randomColor} mb-2`}
+//                     >
+//                       {convertDate}
+//                     </div>
+//                     <p className="text-gray-800 text-sm mb-2">
+//                       {notice.content}
+//                     </p>
+//                     <p className="text-sm text-gray-600">
+//                       {user?.username} / {timeAgo(convertDate)}
+//                     </p>
+//                   </CardContent>
+//                 </Card>
+//               );
+//             })}
+//           </ScrollArea>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NoticeBoard;
+import React from "react";
+
+export const NoticeBoard = () => {
+  return <div>NoticeBoard</div>;
 };
-
-export default NoticeBoard;
