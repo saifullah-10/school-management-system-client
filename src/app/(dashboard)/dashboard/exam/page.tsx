c0c888aa7a6c07f1bfdf83aba737edd7e3e3e08e
Al Rafi <saifullah.bus@gmail.com>
2024-09-17 14:32:47 +0000

    update exam and main navbar design

--- src/app/(dashboard)/dashboard/exam/page.tsx
+++ src/app/(dashboard)/dashboard/exam/page.tsx
@@ -1,3 +1,5 @@
+import Calender from "@/components/dashboard/adminPath/Calendar"
+
 const Exam = () => {
   return (
     <div>
@@ -26,27 +28,27 @@ const Exam = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
+                <tr className=" text-black/60">
                   <td className=" px-3 py-2  ">DSA</td>
                   <td>80</td>
                   <td>10/14/24</td>
                 </tr>
-                <tr>
+                <tr className=" text-black/60">
                   <td className=" px-3 py-2  ">D.Math</td>
                   <td>80</td>
                   <td>10/18/24</td>
                 </tr>
-                <tr>
+                <tr className=" text-black/60">
                   <td className=" px-3 py-2  ">Cyber Security</td>
                   <td>80</td>
                   <td>10/25/24</td>
                 </tr>
-                <tr>
+                <tr className=" text-black/60">
                   <td className=" px-3 py-2  ">Networking</td>
                   <td>80</td>
                   <td>10/27/24</td>
                 </tr>
-                <tr>
+                <tr className=" text-black/60">
                   <td className=" px-3 py-2  ">Computer Arch</td>
 
                   <td>80</td>
@@ -56,7 +58,10 @@ const Exam = () => {
             </table>
           </div>
         </div>
-        <div className="col-span-8 relative h-96 border-2 border-blue-300 overflow-y-scroll ">
+        <div className="col-span-8 relative  h-96 border-2 rounded-xl border-blue-300 custom-scrollbar overflow-y-scroll ">
+          <div className=" my-2">
+            <h1 className="text-center text-xl font-semibold">Weekly Assignment</h1>
+          </div>
           <table className=" w-full relative rounded-xl">
             <thead className=" sticky top-0 bg-blue-300 mx-3 ">
               <tr className=" text-center">
@@ -69,9 +74,9 @@ const Exam = () => {
             <tbody>
               {Array.from({ length: 20 }, (_, index: number) => (
                 <tr key={index} className=" text-center">
-                  <td className=" py-2">1</td>
-                  <td>Weekly Assignment For Dsa </td>
-                  <td>4/12/2024</td>
+                  <td className=" text-black/60 py-2">1</td>
+                  <td  className=" text-black/60" >Weekly Assignment For Dsa </td>
+                  <td className=" text-black/60" >4/12/2024</td>
                   <td className=" py-2 flex justify-center">
                     <button className=" bg-blue-200 border border-blue-600  text-black px-3 py-1 rounded-xl">
                       Start
@@ -99,11 +104,11 @@ const Exam = () => {
             </button>
           </form>
         </div>
-        <div className="col-span-4">
+        <div className="col-span-4 relative custom-scrollbar overflow-y-scroll  h-64 border-2 rounded-xl border-blue-300">
           <div>
-            <h1 className=" text-center my-5 font-semibold ">Exam History</h1>
+            <h1 className=" text-center my-2  font-semibold  ">Exam History</h1>
           </div>
-          <div className=" border-2 border-blue-300 relative  w-full overflow-y-scroll   h-60">
+          <div className=" relative  w-full ">
             <table className=" text-center relative w-full ">
               <thead className="sticky bg-blue-300 mx-3  top-0">
                 <tr className=" text-center">
@@ -124,7 +129,9 @@ const Exam = () => {
             </table>
           </div>
         </div>
-        <div className="col-span-4">Calender</div>
+        <div className="col-span-4">
+          <Calender/>
+        </div>
       </div>
     </div>
   );
--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import { useAuth } from "@/contextProvider/ContextProvider";
+import { logout } from "@/utils/api/api";
+import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 import { IoClose, IoMenu } from "react-icons/io5";
 
+
+
 const Navbar = () => {
   const navigate = usePathname();
+  const [open, setOpen] = useState<boolean>(false)
 
   const Links = (
     <>
@@ -66,8 +72,19 @@ const Navbar = () => {
   if (navigate?.includes("dashboard")) {
     return;
   }
-
+const router = useRouter()
+const auth = useAuth()
+if (auth === null){
+  return <div>Loading...</div>
+}
+const {user, setUser}  = auth;
+const handleLogout = () => {
+  logout();
+  setUser(null);
+  router.push("/login");
+};
   return (
+   <>
     <div
       className={`border-b-[1px] border-[#704FE6] sticky top-0 z-50 bg-white  ${
         navigate === "/login" || navigate === "/register" ? "hidden" : ""
@@ -94,11 +111,16 @@ const Navbar = () => {
           {Links}
         </div>
         {/* Right */}
-        <Link href="/login">
-          <button className="transform transition-transform  hover:scale-110 text-[#704FE6] border-2 border-[#704FE6] font-semibold px-5 py-2 rounded-[8px] hover:bg-[#704FE6] hover:text-white ">
+      
+
+    {user ? (  <div onClick={()=>setOpen(!open)} className=" flex items-center gap-2">
+        <Image className=" rounded-full" src={user?.photoUrl} width={40}  height={40} alt="navbar-logo"/>
+        <span className=" px-2 bg-blue-200 border-2 border-blue-500 rounded-full">{user?.username}</span>
+      </div>): (  <Link href="/login">
+          <button  className="transform transition-transform  hover:scale-110 text-[#704FE6] border-2 border-[#704FE6] font-semibold px-5 py-2 rounded-[8px] hover:bg-[#704FE6] hover:text-white ">
             Login
           </button>
-        </Link>
+        </Link>)}
       </nav>
       {/* Mobile nav */}
       <div
@@ -110,6 +132,16 @@ const Navbar = () => {
         {Links}
       </div>
     </div>
+    {/* nav profile */}
+{
+  open ? (   <div className=" absolute ">
+   <div className="  transition-transform p-3 fixed top-20  flex flex-col gap-3 z-[99] right-20 rounded-xl py-5 bg-white"> <Link  href="/dashboard"><div onClick={()=>setOpen(false)} className="px-2 cursor-pointer border-b-2 border-blue-500"><p>Dashboard</p></div></Link>
+    <div  onClick={()=>{handleLogout(), setOpen(false)}} className="px-2 cursor-pointer border-b-2 border-blue-500"><p>Logout</p></div>
+    </div>
+    
+       </div>): ''
+}
+   </>
   );
 };
 
