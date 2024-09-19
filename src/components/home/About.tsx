fa1c41ab998b51a5c582025704fac64a72f063b6
Al Rafi <saifullah.bus@gmail.com>
2024-09-19 04:59:50 +0000

    update popular course design

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -57,7 +57,7 @@ export default function About() {
             </h1>
           </div>
           <div>
-            <h1 className="text-3xl">
+            <h1 className="text-3xl md:text-5xl font-bold">
               Empowering Education: Our University Management Solution{" "}
             </h1>
           </div>
